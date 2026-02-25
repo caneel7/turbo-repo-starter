c63bd9125e7855c76f87c656ba8dc05826c192ba
caneel7 <caneelmacwan98@gmail.com>
2026-02-26 00:20:54 +0530

    feat: update project structure with dotenv integration and Docker Compose setup

--- app/api/src/constants.ts
+++ app/api/src/constants.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const PORT = process.env.PORT ?? 3000;
 export const FRONTEND_ENDPOINT = process.env.FRONTEND_ENDPOINT ?? 'localhost:3000';
 export const JWT_SECRET = process.env.JWT_SECRET ?? 'secret';
--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,34 +1,37 @@
-import 'dotenv';
+import { FRONTEND_ENDPOINT, JWT_SECRET, PORT } from './constants';
 import { Elysia } from 'elysia';
 import { node } from '@elysiajs/node';
 import { logger } from '@tqman/nice-logger';
 import swagger from '@elysiajs/swagger';
 import cors from '@elysiajs/cors';
-import { FRONTEND_ENDPOINT, JWT_SECRET, PORT } from './constants';
 import bearer from '@elysiajs/bearer';
 import jwt from '@elysiajs/jwt';
+import { prisma } from '@slot/database'
 
 const app = new Elysia({
     adapter: node(),
 })
-.use(cors({
-    origin: FRONTEND_ENDPOINT,
-    credentials: true,
-    methods: ['GET','POST','PUT','DELETE']
-}))
-.use(bearer())
-.use(jwt({
-    secret:JWT_SECRET
-}))
-.use(logger({ mode: 'live', withTimestamp: true }))
-.use(swagger({
-    path: '/docs',
-    documentation: {
-        info: {
-            title: 'Slot API',
-            version: '1',
+    .use(cors({
+        origin: FRONTEND_ENDPOINT,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    }))
+    .use(bearer())
+    .use(jwt({
+        secret: JWT_SECRET
+    }))
+    .use(logger({ mode: 'live', withTimestamp: true }))
+    .use(swagger({
+        path: '/docs',
+        documentation: {
+            info: {
+                title: 'Slot API',
+                version: '1',
+            }
         }
-    }
-}));
+    }));
 
-app.listen(PORT);
+app.listen(PORT, async () => {
+    await prisma.$connect();
+    console.log(`Server is running on port ${PORT}`);
+});
